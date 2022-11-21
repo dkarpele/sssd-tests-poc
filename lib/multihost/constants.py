a9a1f43375882c00d6f05883a0c4c65aab673b34
Denis Karpelevich <dkarpele@redhat.com>
2022-11-21 20:44:08 +0100

    Revert line breaks commit
    Signed-off-by: Denis Karpelevich <dkarpele@redhat.com>

--- lib/multihost/constants.py
+++ lib/multihost/constants.py
@@ -36,8 +36,7 @@ class KnownTopology(Enum):
         name='ldap',
         topology=Topology(TopologyDomain('sssd', client=1, ldap=1, nfs=1)),
         domains=dict(test='sssd.ldap[0]'),
-        fixtures=dict(client='sssd.client[0]', ldap='sssd.ldap[0]',
-                      provider='sssd.ldap[0]', nfs='sssd.nfs[0]'),
+        fixtures=dict(client='sssd.client[0]', ldap='sssd.ldap[0]', provider='sssd.ldap[0]', nfs='sssd.nfs[0]'),
     )
     """
     .. topology-mark:: KnownTopology.LDAP
@@ -47,8 +46,7 @@ class KnownTopology(Enum):
         name='ipa',
         topology=Topology(TopologyDomain('sssd', client=1, ipa=1, nfs=1)),
         domains=dict(test='sssd.ipa[0]'),
-        fixtures=dict(client='sssd.client[0]', ipa='sssd.ipa[0]',
-                      provider='sssd.ipa[0]', nfs='sssd.nfs[0]'),
+        fixtures=dict(client='sssd.client[0]', ipa='sssd.ipa[0]', provider='sssd.ipa[0]', nfs='sssd.nfs[0]'),
     )
     """
     .. topology-mark:: KnownTopology.IPA
@@ -58,8 +56,7 @@ class KnownTopology(Enum):
         name='ad',
         topology=Topology(TopologyDomain('sssd', client=1, ad=1, nfs=1)),
         domains=dict(test='sssd.ad[0]'),
-        fixtures=dict(client='sssd.client[0]', ad='sssd.ad[0]',
-                      provider='sssd.ad[0]', nfs='sssd.nfs[0]'),
+        fixtures=dict(client='sssd.client[0]', ad='sssd.ad[0]', provider='sssd.ad[0]', nfs='sssd.nfs[0]'),
     )
     """
     .. topology-mark:: KnownTopology.AD
@@ -69,8 +66,7 @@ class KnownTopology(Enum):
         name='samba',
         topology=Topology(TopologyDomain('sssd', client=1, samba=1, nfs=1)),
         domains={'test': 'sssd.samba[0]'},
-        fixtures=dict(client='sssd.client[0]', samba='sssd.samba[0]',
-                      provider='sssd.samba[0]', nfs='sssd.nfs[0]'),
+        fixtures=dict(client='sssd.client[0]', samba='sssd.samba[0]', provider='sssd.samba[0]', nfs='sssd.nfs[0]'),
     )
     """
     .. topology-mark:: KnownTopology.Samba
@@ -79,9 +75,9 @@ class KnownTopology(Enum):
 
 class KnownTopologyGroup(Enum):
     """
-    Groups of well-known topologies that can be given to
-    ``pytest.mark.topology`` directly. It is expected to use these values in
-    favor of providing custom marker values.
+    Groups of well-known topologies that can be given to ``pytest.mark.topology``
+    directly. It is expected to use these values in favor of providing
+    custom marker values.
 
     The test is parametrized and runs multiple times, once per each topology.
 
@@ -93,8 +89,7 @@ class KnownTopologyGroup(Enum):
             assert True
     """
 
-    AnyProvider = [KnownTopology.AD, KnownTopology.IPA, KnownTopology.LDAP,
-                   KnownTopology.Samba]
+    AnyProvider = [KnownTopology.AD, KnownTopology.IPA, KnownTopology.LDAP, KnownTopology.Samba]
     """
     .. topology-mark:: KnownTopologyGroup.AnyProvider
     """
--- lib/multihost/utils/sssd.py
+++ lib/multihost/utils/sssd.py
@@ -22,8 +22,7 @@ class HostSSSD(MultihostUtility):
     All changes are automatically reverted when a test is finished.
     """
 
-    def __init__(self, host: MultihostHost, fs: HostFileSystem,
-                 svc: HostService, load_config: bool = False) -> None:
+    def __init__(self, host: MultihostHost, fs: HostFileSystem, svc: HostService, load_config: bool = False) -> None:
         super().__init__(host)
         self.fs = fs
         self.svc = svc
@@ -80,15 +79,13 @@ class HostSSSD(MultihostUtility):
         :type apply_config: bool, optional
         :param check_config: Check configuration for typos, defaults to True
         :type check_config: bool, optional
-        :param debug_level: Automatically set debug level to the given value,
-        defaults to 0xfff0
+        :param debug_level: Automatically set debug level to the given value, defaults to 0xfff0
         :type debug_level:  str | None, optional
         :return: Running SSH process.
         :rtype: SSHProcess
         """
         if apply_config:
-            self.config_apply(check_config=check_config,
-                              debug_level=debug_level)
+            self.config_apply(check_config=check_config, debug_level=debug_level)
 
         return self.svc.async_start(service)
 
@@ -112,15 +109,13 @@ class HostSSSD(MultihostUtility):
         :type apply_config: bool, optional
         :param check_config: Check configuration for typos, defaults to True
         :type check_config: bool, optional
-        :param debug_level: Automatically set debug level to the given value,
-        defaults to 0xfff0
+        :param debug_level: Automatically set debug level to the given value, defaults to 0xfff0
         :type debug_level:  str | None, optional
         :return: SSH process result.
         :rtype: SSHProcessResult
         """
         if apply_config:
-            self.config_apply(check_config=check_config,
-                              debug_level=debug_level)
+            self.config_apply(check_config=check_config, debug_level=debug_level)
 
         return self.svc.start(service, raise_on_error=raise_on_error)
 
@@ -135,8 +130,7 @@ class HostSSSD(MultihostUtility):
         """
         return self.svc.async_stop(service)
 
-    def stop(self, service='sssd', *, raise_on_error: bool = True) \
-            -> SSHProcessResult:
+    def stop(self, service='sssd', *, raise_on_error: bool = True) -> SSHProcessResult:
         """
         Stop SSSD service. The call will wait until the operation is finished.
 
@@ -166,15 +160,13 @@ class HostSSSD(MultihostUtility):
         :type apply_config: bool, optional
         :param check_config: Check configuration for typos, defaults to True
         :type check_config: bool, optional
-        :param debug_level: Automatically set debug level to the given value,
-        defaults to 0xfff0
+        :param debug_level: Automatically set debug level to the given value, defaults to 0xfff0
         :type debug_level:  str | None, optional
         :return: Running SSH process.
         :rtype: SSHProcess
         """
         if apply_config:
-            self.config_apply(check_config=check_config,
-                              debug_level=debug_level)
+            self.config_apply(check_config=check_config, debug_level=debug_level)
 
         return self.svc.async_restart(service)
 
@@ -188,8 +180,7 @@ class HostSSSD(MultihostUtility):
         debug_level: str | None = '0xfff0'
     ) -> SSHProcessResult:
         """
-        Restart SSSD service. The call will wait until the operation is
-        finished.
+        Restart SSSD service. The call will wait until the operation is finished.
 
         :param service: Service to start, defaults to 'sssd'
         :type service: str, optional
@@ -199,20 +190,17 @@ class HostSSSD(MultihostUtility):
         :type apply_config: bool, optional
         :param check_config: Check configuration for typos, defaults to True
         :type check_config: bool, optional
-        :param debug_level: Automatically set debug level to the given value,
-        defaults to 0xfff0
+        :param debug_level: Automatically set debug level to the given value, defaults to 0xfff0
         :type debug_level:  str | None, optional
         :return: SSH process result.
         :rtype: SSHProcessResult
         """
         if apply_config:
-            self.config_apply(check_config=check_config,
-                              debug_level=debug_level)
+            self.config_apply(check_config=check_config, debug_level=debug_level)
 
         return self.svc.restart(service, raise_on_error=raise_on_error)
 
-    def clear(self, *, db: bool = True, config: bool = False,
-              logs: bool = False):
+    def clear(self, *, db: bool = True, config: bool = False, logs: bool = False):
         """
         Clear SSSD data.
 
@@ -262,8 +250,7 @@ class HostSSSD(MultihostUtility):
         host = role.host
 
         if not isinstance(host, ProviderHost):
-            raise ValueError(f'Host type {type(host)} can not be imported as'
-                             f' domain')
+            raise ValueError(f'Host type {type(host)} can not be imported as domain')
 
         self.config[f'domain/{name}'] = host.client
         self.config['sssd'].setdefault('domains', '')
@@ -289,20 +276,17 @@ class HostSSSD(MultihostUtility):
         """
         Load remote SSSD configuration.
         """
-        result = self.host.ssh.exec(['cat', '/etc/sssd/sssd.conf'],
-                                    log_level=SSHLog.Short)
+        result = self.host.ssh.exec(['cat', '/etc/sssd/sssd.conf'], log_level=SSHLog.Short)
         self.config.clear()
         self.config.read_string(result.stdout)
 
-    def config_apply(self, check_config: bool = True,
-                     debug_level: str | None = '0xfff0') -> None:
+    def config_apply(self, check_config: bool = True, debug_level: str | None = '0xfff0') -> None:
         """
         Apply current configuration on remote host.
 
         :param check_config: Check configuration for typos, defaults to True
         :type check_config: bool, optional
-        :param debug_level: Automatically set debug level to the given value,
-        defaults to 0xfff0
+        :param debug_level: Automatically set debug level to the given value, defaults to 0xfff0
         :type debug_level:  str | None, optional
         """
         cfg = self.__set_debug_level(debug_level)
